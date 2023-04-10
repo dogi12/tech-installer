68a15b1f0d2b2a85a7b2f914138f84c7f8363e6f
dogi12 <88685766+dogi12@users.noreply.github.com>
2023-04-10 13:33:06 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,19 @@
 print("loaded github")
+
+
+local net = require 'net'
+local string =  require 'string'
+local sys = require 'sys'
+
+
+function wait(time)
+
+sys.sleep(time*1000)
+
+end
+
+
+wait(1)
+
+
+print("loading variables")
